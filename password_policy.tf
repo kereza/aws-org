f8bbee90670855f224ffa630525eec92c7d5398f
Ivan Kerezov <ivan.kerezov@tis.biz>
2021-08-05 22:14:57 +0200

    first commit

---
+++ password_policy.tf
@@ -0,0 +1,10 @@
+resource "aws_iam_account_password_policy" "tis" {
+  minimum_password_length        = 10
+  require_lowercase_characters   = true
+  require_uppercase_characters   = true
+  require_numbers                = true
+  require_symbols                = true
+  allow_users_to_change_password = true
+  password_reuse_prevention      = 24
+  max_password_age               = 90
+}
