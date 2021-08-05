7af81a7ab32612524fa0b75a3218f27fa622fa69
Ivan Kerezov <ivan.kerezov@tis.biz>
2021-08-05 21:30:35 +0200

    first commit

---
+++ aws_org_roles.tf
@@ -0,0 +1,36 @@
+data "aws_iam_policy_document" "aws_organization_root_trust" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${var.master_acc_id}:root"]
+    }
+  }
+}
+
+
+########
+
+resource "aws_iam_role" "sub_acc_roles" {
+  for_each             = toset(var.sub_roles)
+  description          = "Roles for subaccounts"
+  name                 = "OrganizationAccount${each.value}AccessRole"
+  path                 = "/"
+  max_session_duration = var.max_session_duration
+  assume_role_policy   = data.aws_iam_policy_document.aws_organization_root_trust.json
+}
+
+resource "aws_iam_policy" "tis_sub_acc_boundary_role_policy" {
+  description = "Main Boundary Policy, which need to be attached to every UserManage role"
+  name        = "MainBoundaryPolicy"
+  path        = "/"
+  policy      = templatefile("${path.module}/role_policies/MainBoundary.json", {})
+}
+
+resource "aws_iam_role_policy" "tis_sub_acc_role_policy" {
+  for_each = toset(var.sub_roles)
+  name     = each.value
+  role     = aws_iam_role.sub_acc_roles[each.value].id
+  policy   = templatefile("${path.module}/role_policies/${each.value}.json", { account_id = var.account_id })
+}
--- variables.tf
+++ variables.tf
@@ -1,7 +1,28 @@
+variable "master_acc_id" {
+  description = "The ID of the master AWS acc"
+  type = "string"
+  default = "131845455848"
+}
+
+/*
+For the example we have only ONE AWS account that is why - same value
+*/
+variable "account_id" {
+  description = "The ID of the sub AWS acc"
+  type = "string"
+  default = "131845455848"
+}
+
 variable "iam_groups_acc_map" {
   description = "Map of IAM groups and in which AWS accounts they can ASSSUME Roles"
   default = {
     "CloudOps"    = ["131845455848"]
     "Developers"  = ["131845455848"]
   }
+}
+
+variable "sub_roles" {
+    description = "List of roles to be created in the AWS acc"
+    type = "list"
+    default = ["DevOps", "Developers"]
 }
