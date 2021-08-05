3cc56a16e930d6a088219e19a521776b335928a3
Ivan Kerezov <ivan.kerezov@tis.biz>
2021-08-05 22:09:56 +0200

    first commit

--- iam_groups.tf
+++ iam_groups.tf
@@ -7,6 +7,14 @@ resource "aws_iam_group" "groups" {
   name     = each.key
 }
 
+# MFA Policy so every user/group can manage its own MFA device
+# Attached to every IAM Group
+resource "aws_iam_policy" "aws_organization_self_manage" {
+  name   = "SelfManageMFA"
+  path   = "/"
+  policy = templatefile("${path.module}/group_policies/self_manage.json", {})
+}
+
 # Default Policy for every IAM Group
 resource "aws_iam_policy" "default_org_policy" {
   for_each = var.iam_groups_acc_map
@@ -20,5 +28,12 @@ resource "aws_iam_group_policy_attachment" "org_default" {
   for_each   = var.iam_groups_acc_map
   group      = each.key
   policy_arn = aws_iam_policy.default_org_policy[each.key].arn
+  depends_on = [aws_iam_group.groups]
+}
+
+resource "aws_iam_group_policy_attachment" "self_manage" {
+  for_each   = var.iam_groups_acc_map
+  group      = each.key
+  policy_arn = aws_iam_policy.aws_organization_self_manage.arn
   depends_on = [aws_iam_group.groups]
 }
